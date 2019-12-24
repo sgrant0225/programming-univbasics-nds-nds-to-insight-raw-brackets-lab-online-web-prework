99cfd24db5abc082bcff639ff4d67dc1232de551
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-24 04:51:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ def directors_totals(nds)
 directors_totals = {} #returns a hash describing directors_totals- so I made a new hash
  row_index = 0 #This row_index iterate thorugh the whole array
  while row_index < nds.length do
-  directors_name = nds[row_index][:name] # this pulls the values out of name hash array
+  directors_name = nds[row_index][:name] # this pulls the values out of name hash array and sets equal it the new variable
   directors_totals[directors_name] = 0 #set that result to the directors_totals hash
   movie_index = 0 #This is a counter method that loops through all of the movie hashes
   while movie_index < nds[row_index][:movies].length do

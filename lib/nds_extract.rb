6ebe95d540a3d65f94c76be2714fb9708ed9a342
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-24 04:48:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,12 +3,12 @@ require 'directors_database'
 
 def directors_totals(nds)
   # pp nds
-directors_totals = {}
- row_index = 0 
+directors_totals = {} #returns a hash describing directors_totals- so I made a new hash
+ row_index = 0 #This row_index iterate thorugh the whole array
  while row_index < nds.length do
-  directors_name = nds[row_index][:name]
-  directors_totals[directors_name] = 0
-  movie_index = 0
+  directors_name = nds[row_index][:name] # this pulls the values out of name hash array
+  directors_totals[directors_name] = 0 #set that result to the directors_totals hash
+  movie_index = 0 #This is a counter method that loops through all of the movie hashes
   while movie_index < nds[row_index][:movies].length do
   directors_totals[directors_name] += nds[row_index][:movies][movie_index][:worldwide_gross]
   movie_index += 1

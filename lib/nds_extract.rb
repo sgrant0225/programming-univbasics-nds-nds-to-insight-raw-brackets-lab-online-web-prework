6a16984ee214dfc8e6b2a8d6c10abf72bd219f51
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-24 03:51:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,9 +7,7 @@ hash.new = [directors_totals]
  grand_total = 0 
  row_index = 0 
  while row_index < directors_database[:name].length do
-  # column_index = 0  
-  # while column_index  < directors_database[row_index].length do 
- puts directors_database[:name]
+   puts directors_database[:name]
   
   end
   row_index += 1

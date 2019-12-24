d5a2cff2f44f390db2996db19e95c778d217e4d5
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-24 00:42:04 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,9 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+pp nds
+ grand_total = 0 
+ row_index = 0 
+ while row_index < nds.length do
+  column_index = 0  
+  while column_index  < nds[row_index].length do 
+  directors_database[:name]
+  
   # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
+  # that you know what you're starting with
+  
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
@@ -19,4 +27,6 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   nil
+end
+end
 end
